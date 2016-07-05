ef58ce0eef2b8f9058eca99593e3cc565e541dd8
Javier Merchán <merchan1395@gmail.com>
2016-07-05 18:55:53 -0400

    arreglo bootstrap
    .

--- Trendloft/footer.php
+++ Trendloft/footer.php
@@ -53,12 +53,15 @@
 </section>
 </div>
 
+  <script src="resources/bootstrap/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
+
 <?php
-$mysqli = new mysqli("127.0.0.1", "root", "", "wordpress", 3306);
-if ($mysqli->connect_errno) {
-  echo "Fallo al contenctar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
+//$mysqli = new mysqli("127.0.0.1", "root", "", "wordpress", 3306);
+//if ($mysqli->connect_errno) {
+  //echo "Fallo al contenctar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+//}
 ?>
--- Trendloft/header.php
+++ Trendloft/header.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>TrendLoft</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>TrendLoft</title>
+
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto+Condensed|Montserrat|Open+Sans">
+
+  <link rel="stylesheet" href="resources/bootstrap/css/bootstrap.min.css">
 
 	<link rel="stylesheet" href="resources/css/estilos.css">
+
+
 	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 	<script type="text/javascript" src="resources/js/scroll.js"></script>
 	<script type="text/javascript" src="resources/js/stock.js"></script>
-
-
-	<link rel="stylesheet" type="text/css" href="resources/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="resources/bootstrap/js/bootstrap.min.js">
-	<link rel="stylesheet" type="text/css" href="resources/bootstrap/css/bootstrap-theme.min.css">
 
 </head>
 
