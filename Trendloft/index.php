4aadcea153bd03a2a556091b4b83f0eb5502f6fb
Javier Merchán <merchan1395@gmail.com>
2016-07-05 00:08:03 -0400

    Adaptacion a lo que teniamos en WP
    .

---
+++ Trendloft/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>TrendLoft</title>
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto+Condensed|Montserrat|Open+Sans">
+
+	<link rel="stylesheet" href="resources/css/estilos.css">
+	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="resources/js/scroll.js"></script>
+	<script type="text/javascript" src="resources/js/stock.js"></script>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+	<div class="overlay">
+		<section>
+			<header>
+				<div class="wrapper">
+					<div class="logo"><img src="resources/images/Main_TL_Logo_NoName.png" alt=""></div>
+					<nav class="menu">
+						<a href="#home">Home</a>
+						<a href="#about">About Us</a>
+						<a href="#info">Contact</a>
+					</nav>
+          <nav class="social">
+						<a href="http://facebook.com/trendloft"><img src="resources/images/facebookIco.png" alt=""></a>
+						<a href="http://twitter.com/trendloft"><img src="resources/images/twitterIco.png" alt=""></a>
+						<a href="http://instagram.com/trend_loft"><img src="resources/images/instagramIco.png" alt=""></a>
+            <a href="http://pintrest.com/trendloft"><img src="resources/images/pinterestIco.png" alt=""></a>
+            <a href="mailto:info@trendloft.net"><img src="resources/images/mailIco.png" alt=""></a>
+					</nav>
+				</div>
+			</header>
+
+			<div class="home wrapper" id="home">
+					<div class="titulo">
+						<h1>Comfort at First Sight...</h1>
+					</div>
+			</div>
+
+			<div class="about wrapper" id="about">
+				<p>
+					<h2>About Us</h2>
+
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+
+					Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut
+
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+				</p>
+			</div>
+
+
+			<footer>
+				<div class="info wrapper" id="info">
+					<div class="row icono">
+						<div class="col-md-4"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>
+						</div>
+						<div class="col-md-4"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
+						</div>
+						<div class="col-md-4"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4"><p>+1 (786) 660-4619</p>
+						</div>
+						<div class="col-md-4"><p>500 NE 185th St. Miami, FL. 33179</p>
+						</div>
+						<div class="col-md-4"><p>salmonsalmonseafood@gmail.com</p>
+						</div>
+					</div>
+
+
+				</div>
+			</footer>
+
+		</section>
+	</div>
+
+</body>
+
+</html>
