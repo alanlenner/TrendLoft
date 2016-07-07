67c0632d91cadfc91a552ad2e4aeb4ee0b084f96
Javier Merchán <merchan1395@gmail.com>
2016-07-07 01:28:34 -0400

    Arreglo Conexión a BD
    .

--- Trendloft/header.php
+++ Trendloft/header.php
@@ -19,6 +19,33 @@
 
 </head>
 
+<!-- CONEXION A BASE DE DATOS -->
+<?php
+  $user = 'root';
+  $password = 'root';
+  $db = 'trendloft';
+  $host = 'localhost';
+  $port = 8889;
+
+  $link = mysql_connect("$host:$port", $user, $password) or die('Could not connect to server.' );
+  $db_selected = mysql_select_db($db, $link)or die('Could not select database.');
+
+/* CODIGO PARA HACER CONSULTAS A BD
+
+
+  $sql = "SELECT correo FROM usuarios";
+  $res = mysql_query($sql, $link);
+
+  while ($row = mysql_fetch_array($res)) {
+   $campo = $row["correo"];
+   echo $campo;
+  }
+
+
+*/
+?>
+
+
 <body class="body">
 	<div class="overlay">
 		<section>
